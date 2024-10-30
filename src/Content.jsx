12d9222d20a5c60834c6fdf79dfda033084bb44d
Josias Weber <josias@clae.com.br>
2024-10-30 11:54:25 -0300

    ajusta o caminho do logo

--- src/Content.jsx
+++ src/Content.jsx
@@ -239,7 +239,7 @@ const Content = () => {
             <Row className="justify-content-center">
               <img
                 alt="logo"
-                src="assets/fullLogo.png"
+                src="assets/logo2.png"
                 style={{
                   width: 230,
                 }}
@@ -255,7 +255,7 @@ const Content = () => {
             <NavbarBrand href="/">
               <img
                 alt="logo"
-                src="./src/assets/fulllogo.png"
+                src="./src/assets/logo2.png"
                 style={{
                   width: 250,
                 }}
